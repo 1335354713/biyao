e32035eec8c437a2dcbfc3bc12348f49bde181d2
zhuquan <1335354713@.com>
2019-11-06 22:02:28 +0800

    day3

---
+++ biyao-webapp-api/controllers/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+const biyaoindex = require("../models/biyaoindex")
+
+router.all("*",(req,res,next)=>{
+  res.header("Access-Control-Allow-Origin","*");
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  if(req.method=="OPTIONS") res.send(200);/*让options请求快速返回*/
+  else  next();
+})
+
+/* GET home page. */
+router.get("/biyaoindex",(req,res)=>{
+  let data=biyaoindex.getData();
+  res.json(data);
+})
+
+module.exports = router;
---
+++ biyao-webapp-api/models/biyaoindex.js
@@ -0,0 +1,41 @@
+module.exports={
+    getData(){
+        let data={
+            "mainLists":[
+                {
+                    "img":"static/rBACVF2jzNyAMrmQAAHaOSE2_0M377_360x360.jpg",
+                    "original":"￥119",
+                    "Present":"219",
+                    "address":"Hugo Boss产品产地",
+                    "describe":"四季款多纤维混纺抗皱衬衫",
+                    "praise":"1690条好评"
+                },
+                {
+                    "img":"static/rBACVF2Nb9KAeR9VAAk_zW3-ryE111_360x360.jpg",
+                    "original":"￥299",
+                    "Present":"399",
+                    "address":"Max Mara产品产地",
+                    "describe":"双排扣领纯色毛呢大衣",
+                    "praise":"100条好评"
+                },
+                {
+                    "img":"static/rBACVFtyMuqAJYZ3AACcR2Yf4PQ442.jpg",
+                    "original":"￥59",
+                    "Present":"89",
+                    "address":"John Lewis产品产地",
+                    "describe":"北欧清新 天鹅绒面料 手感柔软",
+                    "praise":"216条好评"
+                },
+                {
+                    "img":"static/rBACVF1o5kWAaqfHAAbrslpPfyQ508_360x360.jpg",
+                    "original":"￥69",
+                    "Present":"99",
+                    "address":"希尔顿酒店床品供货产地",
+                    "describe":"全棉双边工艺/纤维填充",
+                    "praise":"1966条好评"
+                }
+                ]
+        }
+        return data
+    }
+}
